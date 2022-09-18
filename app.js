29b8cff3467e7e97385ba22f6121a541d1eecb22
Brandon <kounse@gmail.com>
2022-09-18 10:04:09 -0400

    Created four arithmetic functions for add, subtract, multiply, and divide

---
+++ app.js
@@ -0,0 +1,17 @@
+//create add, subtract, multiply, and divide functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
